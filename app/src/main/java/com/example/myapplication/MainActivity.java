9b5b4b7386e8c8dc03a4b89dcff96e2549de9082
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-05-25 15:34:19 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -34,19 +35,75 @@ public class MainActivity extends AppCompatActivity {
 
     private RadioGroup sexRadio;
     private Spinner spinner;
+    RadioButton selectedRadioButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        spinner = (Spinner) findViewById(R.id.spinnerNowUser);
         database=getDatabase();//获得数据库对象
-        final List<String> datas = new ArrayList<>();
-        datas.add("zhangsan");
-        datas.add("lisi");
-        datas.add("wangwu");
+        getAllUserNamesFromDatabase();
+        // 监听 Spinner 选择事件
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 根据选择的用户名查询用户信息并展示到界面上
+                String selectedUserName = parent.getItemAtPosition(position).toString();
+                showUserInfo(selectedUserName);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // 如果没有选择，则不进行任何操作
+            }
+        });
         sexRadio = findViewById(R.id.choosesex);
+    }
+
+    // 根据用户名从数据库中查询用户信息并展示到界面上
+    @SuppressLint("Range")
+    // 根据用户名从数据库中查询用户信息并展示到界面上
+    private void showUserInfo(String userName) {
+        getText();
+        Cursor cursor = database.rawQuery("SELECT * FROM t_user WHERE user_Name = ?", new String[]{userName});
+        if (cursor.moveToFirst()) {
+            getName.setText(cursor.getString(cursor.getColumnIndex("user_Name")));
+            getPassword.setText(cursor.getString(cursor.getColumnIndex("user_Password")));
+            getTel.setText(cursor.getString(cursor.getColumnIndex("user_Tel")));
+            getEmail.setText(cursor.getString(cursor.getColumnIndex("user_Email")));
+            int sex = cursor.getInt(cursor.getColumnIndex("user_Sex"));
+            if (sex == 0) {
+                sexRadio.check(R.id.sex_boy);
+            } else {
+                sexRadio.check(R.id.sex_girl);
+            }
+        }
+        selectedRadioButton = findViewById(sexRadio.getCheckedRadioButtonId());
+        if(selectedRadioButton.getText().toString().equals("男")){
+            getSex = 0; // 假设男性用 0 表示
+        } else {
+            getSex = 1; // 假设女性用 1 表示
+        }
+        cursor.close();
+    }
+    public void getAllUserNamesFromDatabase() {
+        ArrayList<String> userNames = new ArrayList<>();
+        // 在这里执行查询数据库的操作，获取所有用户名数据，并存储到userNames中
+        Cursor cursor = database.rawQuery("SELECT user_Name FROM t_user", null);
+        if (cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range") String userName = cursor.getString(cursor.getColumnIndex("user_Name"));
+
+                userNames.add(userName);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, userNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // 设置适配器给Spinner
+        spinner.setAdapter(adapter);
     }
 
     private void getText(){
@@ -56,13 +113,10 @@ public class MainActivity extends AppCompatActivity {
         deleteButton = (Button) findViewById(R.id.buttondeltet);
         emptyButton = (Button) findViewById(R.id.buttonblank);
         //性别选项
-        spinner = (Spinner) findViewById(R.id.spinnerNowUser);
-
         getName = (EditText) findViewById(R.id.editUserName);
         getPassword = (EditText) findViewById(R.id.editPassword);
         getTel = (EditText) findViewById(R.id.editTel);
         getEmail = (EditText) findViewById(R.id.editEmail);
-        getSex = sexRadio.getCheckedRadioButtonId();
     }
     //为“添加”按钮点击点击事件
     public void buttonAdd_Click(View view){
@@ -83,7 +137,12 @@ public class MainActivity extends AppCompatActivity {
         else if(getSex<0){
             Toast.makeText(MainActivity.this,"请选择性别！",Toast.LENGTH_LONG).show();
         }else{
-
+            selectedRadioButton = findViewById(sexRadio.getCheckedRadioButtonId());
+            if(selectedRadioButton.getText().toString().equals("男")){
+                getSex = 0; // 假设男性用 0 表示
+            } else {
+                getSex = 1; // 假设女性用 1 表示
+            }
             //将数据保存到数据库
             ContentValues values = new ContentValues();
             values.put("user_Name",getName.getText().toString());
@@ -117,7 +176,12 @@ public class MainActivity extends AppCompatActivity {
         else if(getSex<0){
             Toast.makeText(MainActivity.this,"请选择性别！",Toast.LENGTH_LONG).show();
         }else{
-
+            selectedRadioButton = findViewById(sexRadio.getCheckedRadioButtonId());
+            if(selectedRadioButton.getText().toString().equals("男")){
+                getSex = 0; // 假设男性用 0 表示
+            } else {
+                getSex = 1; // 假设女性用 1 表示
+            }
             //将数据保存到数据库
             ContentValues values = new ContentValues();
             values.put("user_Name",getName.getText().toString());
